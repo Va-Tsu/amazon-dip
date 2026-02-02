15f5702cf5079351d171ffcaa0168f2b8343ad92
Oskar <proton2006tom@gmail.com>
2026-02-03 01:56:31 +0200

    deleted indexs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,18 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 //#region Imports
+import { Route, Routes } from "react-router-dom";
 import { Login } from "./pages/Auth/Login/Login"
-import { Register } from "./pages/Auth/Register/Register"
+import { Register } from './pages/Auth/Register/Register'
 import { Welcome } from "./pages/Auth/Welcome/Welcome";
 //#endregion
 function App() {
 
   return (
-    //<Register />
-    // <Login />
-    <Welcome />
+    <Routes>
+      <Route path="/register" element={<Register />}/>
+      <Route path="/login" element={<Login />}/>
+      <Route path="/welcome" element={<Welcome />}/>
+    </Routes>
   )
 }
 
