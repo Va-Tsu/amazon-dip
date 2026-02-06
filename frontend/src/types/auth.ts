cb27746bc4fe45f59984f6725f4fec4d784462ee
Oskar <proton2006tom@gmail.com>
2026-02-06 17:20:51 +0200

    added auth types

---
+++ frontend/src/types/auth.ts
@@ -0,0 +1,17 @@
+export type LoginRequest = {
+  email: string;
+  password: string;
+};
+
+export type RegisterRequest = {
+  email: string;
+  password: string;
+};
+
+export type AuthResponse = {
+  token: string;
+  user: {
+    id: number;
+    email: string;
+  };
+};
